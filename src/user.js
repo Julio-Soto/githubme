0bd4f9198d6efc814fb215907a8d0a113a93ea8c
Julio Soto <juliosotodev@gmail.com>
2022-07-22 20:03:45 -0600

    logo and basic data

--- src/user.js
+++ src/user.js
@@ -4,11 +4,12 @@ import githubLogo from './github-logo.svg';
 
 function User() {
     const [name, setName] = useState('')
-    const [company, setCompany] = useState('')
+    const [company, setCompany] = useState('Company')
+    const [avatar_url,setAvatar_url] = useState('')
+    const [login, setLogin] = useState('')
 
     const navigateParams = useLocation()
     const { userName } = navigateParams.state
-    //console.log(state)
 
     useEffect(() => {
         getData()
@@ -19,18 +20,29 @@ function User() {
 
         const data = await rawData.json()
         setName(data.name)
-        setCompany(data.company)
+        if(data.company) setCompany(data.company)
+        setAvatar_url(data.avatar_url)
+        setLogin(data.login)
     }
 
 
     return (
-        <div>
-            <div>
-                <img src={githubLogo} />
-                <span>GitHub ME</span>
+        <div className='container'>
+            <div className='title'>
+                <img src={githubLogo} alt='github-logo'/>
+                <span className='title-text'>GitHub ME</span>
             </div>
-            <h2>{ name }</h2>
-            <h2>{company}</h2>
+            <div class='img-container'>
+		        <img src={avatar_url} className='user-img' alt='' />
+	        </div>
+            <h1 className='user-name'>{name}</h1>
+	        <h2 className='user-login'>@{login}</h2>
+            <div className='personal-info'>
+                <span class='personal-item'>
+                    <span class="material-icons md-em">business</span>
+                    <span class='company-name info-text'>{company}</span>
+		        </span>
+            </div>
         </div>
     )
 }
