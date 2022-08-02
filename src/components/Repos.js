2f7adae3b6c8b093050f201d06d8238ac24ddb0e
Julio Soto <juliosotodev@gmail.com>
2022-08-01 21:27:14 -0600

    repo boxes CSS and data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,14 +1,31 @@
-
+import '../styles/repos.css'
 
 function Repos( {reposData} ) {
 
     return(
-        <div>
+        <div className='repo-list-wrap'>
+        <div className='repo-list'>
             <ul>
                 {reposData.map(repo => (
-                    <li>{repo.name}</li>                    
+                    <li><a href={repo.svn_url}>
+                    <div class='repo-info'>
+                        <div class='repo-name'>{repo.name}</div>
+                        <p class='repo-description'>{repo.description} </p>
+                        <div class='stats'>
+                            <div class='stats-left'>
+                                <span>{repo.language}</span>
+                                <span>{repo.stargazers_count}</span>
+                                <span>{repo.forks_count}</span>
+                            </div>
+                            <div class='stats-right'>
+                                <span>{repo.size.toLocaleString()}Kb</span>
+                            </div>
+                        </div>
+                    </div>
+                </a></li>                    
                 ))}
             </ul>
+        </div>
         </div>
     )
 }
