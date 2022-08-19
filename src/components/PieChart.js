0a8c40b2f01b57131bb9dd6097428e1f0bd0da25
Julio Soto <juliosotodev@gmail.com>
2022-08-19 01:01:47 -0600

    header to pie chart and fixed bug in pie data calculation

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,62 +1,28 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import '../styles/chart.css'
+import pieChartIcon from '../pie-chart.svg'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
 
-
-function PieChart( { allRepos } ) {
-
-    let languages = new Map()
-
-    for(let i=0; i < allRepos.length; ++i){
-        if(allRepos[i].language){
-            if( languages.has(allRepos[i].language) )
-                languages.set(allRepos[i].language, languages.get(allRepos[i].language) + 1)
-            else
-                languages.set(allRepos[i].language,1) 
-        }
-    }
-
-    const pieLabels = Array.from(languages.keys())
-    const pieData = Array.from(languages.values())
+function PieChart( { pieData } ) {
+    
+    console.table(pieData)
 
     return(
         <div className='chart-container'>
             <div className='pie-container'>
+            <div className='graph-header'>
+            <img src={pieChartIcon} alt='Pie Chart Icon'/>
+			    <span className='graph-title' >Top Languages</span>
+		    </div>
                 <Pie data={{
-                            labels: pieLabels,
+                            labels: pieData.labels,
                             datasets: [
                                 {
                                 label: 'Language',
-                                data: pieData,
+                                data: pieData.data,
                                 backgroundColor: [
                                     'rgba(255, 99, 132, 0.5)',
                                     'rgba(54, 162, 235, 0.5)',
--- src/user.js
+++ src/user.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import Testdata  from './userTestData'
-import RepoTestData from './repoTestData'
+//import Testdata  from './userTestData'
+//import RepoTestData from './repoTestData'
 import Userinfo  from './components/Userinfo'
 import Repos from './components/Repos'
 import PieChart from './components/PieChart'
@@ -9,16 +9,16 @@ import PieChart from './components/PieChart'
 function User() {
     const [userData,setUserData] = useState(null)
     const [reposData, setReposData] = useState([])
-    const [allRepos, setAllRepos] = useState(null)
+    const [pieData, setPieData] = useState({})
     
     
     const navigateParams = useLocation()
     const { userName } = navigateParams.state
 
     useEffect(() => {
-        //getUserData()
+        getUserData()
         getRepoData()
-        setUserData(Testdata)
+        //setUserData(Testdata)
     },[])
 
     const getUserData = async () => {
@@ -29,10 +29,29 @@ function User() {
     }
 
     const getRepoData = async () => {
-        //const rawData = await fetch('https://api.github.com/users/' + userName + "/repos?per_page=100")
-        //const data = await rawData.json()
-        const data = RepoTestData
-        
+        const rawData = await fetch('https://api.github.com/users/' + userName + "/repos?per_page=100")
+        const data = await rawData.json()
+        //const data = RepoTestData
+
+        let languages = new Map()
+
+        console.log(data.length)
+        for(let i=0; i < data.length; ++i){
+            if(data[i].language){
+                if( languages.has(data[i].language) )
+                    languages.set(data[i].language, languages.get(data[i].language) + 1)
+                else
+                    languages.set(data[i].language,1) 
+            }
+        }
+
+        const pieLabels = Array.from(languages.keys())
+        const pieData = Array.from(languages.values())
+
+        setPieData({labels: pieLabels,
+                    data: pieData
+        })
+
         const sortedRepos = data
         .sort( (a,b) => {
             return b.stargazers_count - a.stargazers_count
@@ -40,13 +59,12 @@ function User() {
         .splice(0,8)
 
         setReposData(sortedRepos) 
-        setAllRepos(data)
     }
 
     return (
         <div>
             { userData &&  <Userinfo userData={userData}/> }
-            { allRepos &&  <PieChart allRepos={allRepos}/> }
+            { pieData &&  <PieChart pieData={pieData}/> }
             { reposData && <Repos reposData={reposData}/> }
         </div>
     )
